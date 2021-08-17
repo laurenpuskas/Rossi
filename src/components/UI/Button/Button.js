579d4992f4149fb3ae38d5a672e86cb657b8e04b
Liliripe <liliripe.studio@gmail.com>
2021-08-17 17:09:50 -0500

    netlify form test 2

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -4,35 +4,33 @@ import AniLink from 'gatsby-plugin-transition-link/AniLink'
 import * as styles from './style.module.scss'
 
 const Button = (props) => {
-  if (props.type === 'link') {
+  if (props.type === 'submit') {
+    return <button type="submit">{props.label}</button>
+  } else {
     return (
       <AniLink
         to={props.href}
         transition={props.transition}
         className={`
-        ${props.size == 'large' ? styles.large : styles.medium}
-        ${props.classes}
-        bg-opacity-100
-        hover:bg-opacity-80 
-        transition-all
-        duration-500
-        ease-in-out
-        ${
-          props.open
-            ? 'bg-dark text-white'
-            : props.color == 'black'
-            ? 'bg-dark text-white'
-            : 'bg-white text-dark'
-        }
-      `}
+          ${props.size == 'large' ? styles.large : styles.medium}
+          ${props.classes}
+          bg-opacity-100
+          hover:bg-opacity-80 
+          transition-all
+          duration-500
+          ease-in-out
+          ${
+            props.open
+              ? 'bg-dark text-white'
+              : props.color == 'black'
+              ? 'bg-dark text-white'
+              : 'bg-white text-dark'
+          }
+        `}
       >
         {props.children}
       </AniLink>
     )
-  }
-
-  if (props.type === 'submit') {
-    return <button type="submit">{props.label}</button>
   }
 }
 
