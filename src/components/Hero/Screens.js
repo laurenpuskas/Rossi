123040117904086397b734036138ef210ae5795e
Liliripe <liliripe.studio@gmail.com>
2021-09-05 14:23:40 -0500

    init hero completed (initial)

--- src/components/Hero/Screens.js
+++ src/components/Hero/Screens.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
+
+import * as style from './style.module.scss'
 
 const Screens = () => {
-  return <div>screens</div>
+  return (
+    <section>
+      <StaticImage
+        src="../../images/hero/screen1.png"
+        className={style.screen}
+      />
+      <StaticImage
+        src="../../images/hero/screen1.png"
+        className={style.screen}
+      />
+    </section>
+  )
 }
 
 export default Screens
