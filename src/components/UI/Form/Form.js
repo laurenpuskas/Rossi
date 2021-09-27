9f1c7eacfe16f6f46c78e1bf83c6c740d1032c10
Liliripe <liliripe.studio@gmail.com>
2021-09-27 13:55:07 -0500

    new form company

--- src/components/UI/Form/Form.js
+++ src/components/UI/Form/Form.js
@@ -5,8 +5,12 @@ import * as style from './style.module.scss'
 
 const Form = (props) => {
   return (
-    <form name="contact" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact" />
+    <form
+      name="contact"
+      method="POST"
+      action="https://getform.io/f/23f13765-d671-4302-8516-4ea5378a5e8a"
+    >
+      <input type="text" name="name" placeholder="Type your name" />
       <input type="email" name="email" placeholder="Type your email" />
       <button type="submit">Send</button>
     </form>
