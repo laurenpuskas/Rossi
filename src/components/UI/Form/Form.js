eff82fabfeeaa48c4a71dc11aba1f4f4af677420
Liliripe <liliripe.studio@gmail.com>
2021-09-27 14:00:19 -0500

    new form fix

--- src/components/UI/Form/Form.js
+++ src/components/UI/Form/Form.js
@@ -6,13 +6,15 @@ import * as style from './style.module.scss'
 const Form = (props) => {
   return (
     <form
-      name="contact"
+      name={props.name}
       method="POST"
       action="https://getform.io/f/23f13765-d671-4302-8516-4ea5378a5e8a"
+      className={`${props.classes ? props.classes : ''} ${style.form}`}
     >
-      <input type="text" name="name" placeholder="Type your name" />
-      <input type="email" name="email" placeholder="Type your email" />
-      <button type="submit">Send</button>
+      {props.children}
+      <Button type={`submit`} orange arrow>
+        {props.send}
+      </Button>
     </form>
   )
 }
