0a0f2c97b2f80a1ed6238c784e4d3b98e2904077
Liliripe <liliripe.studio@gmail.com>
2021-10-11 17:40:28 -0500

    init hero updates

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,29 +1,15 @@
 import React from 'react'
-import { motion } from 'framer-motion'
 
-import Loading from '../UI/Loading'
 import Header from './Header'
 import * as style from './style.module.scss'
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Loading title="Rossi." />
-      <motion.div
-        className={style.content}
-        animate={{
-          opacity: ['0%', '100%'],
-        }}
-        transition={{
-          duration: 8,
-          ease: 'easeInOut',
-          times: [0, 1],
-          loop: false,
-        }}
-      >
+      <div className={style.content}>
         <Header />
         <div className={style.children}>{children}</div>
-      </motion.div>
+      </div>
     </>
   )
 }
